b7da837fbca432788a289be257afee97d1dc81da
lucija0905 <lm20190223@student.fon.bg.ac.rs>
2023-01-10 13:20:38 +0100

    REST API autentifikacija putem sanctuma

--- laravelzadatak/routes/api.php
+++ laravelzadatak/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\SalonController;
 use App\Http\Controllers\API\MusterijaController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,11 @@ use App\Http\Controllers\API\MusterijaController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('salon', SalonController::class);
-Route::resource('musterija', MusterijaController::class);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::resource('salon', SalonController::class);
+    Route::resource('musterija', MusterijaController::class);
 });
